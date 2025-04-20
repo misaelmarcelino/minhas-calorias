d1d63e193b4f48c23e7895f5c6ec3074c9f4b17e
Misael Marcelino <misael.marcelino@outlook.com>
2025-04-20 00:29:55 -0300

    iniciando projeto com Spring boot

---
+++ src/main/java/br/com/fiap/minhas_calorias/MinhasCaloriasApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.minhas_calorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhasCaloriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhasCaloriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/minhas_calorias/MinhasCaloriasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.minhas_calorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasCaloriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
